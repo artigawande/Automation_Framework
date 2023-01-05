f8df1d9ca0e34403607457f81807dcd1d2a74d66
artig <artig@maveric-systems.com>
2023-01-05 15:04:36 +0530

    Add Employee

---
+++ EmployeeManagement/EmployeeTest.cs
@@ -0,0 +1,45 @@
+﻿using EmployeeManagement.Base;
+using EmployeeManagement.Utilities;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EmployeeManagement
+{
+    public class EmployeeTest:AutomationWrapper
+    {
+        [Test, TestCaseSource(typeof(DataSource), nameof(DataSource.AddValidEmployeeTest1))]
+        public void AddValidEmployeeTest(string username, string password, string FirstName, string MiddleName, string LastName, string ActualResult)
+        {
+            driver.FindElement(By.XPath("//input[@placeholder='Username']")).SendKeys(username);
+            driver.FindElement(By.XPath("//input[@type='password']")).SendKeys(password);
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+            driver.FindElement(By.XPath("//span[text()='PIM']")).Click();
+
+            driver.FindElement(By.XPath("//a[text()='Add Employee']")).Click();
+
+            driver.FindElement(By.XPath("//input[@placeholder='First Name']")).SendKeys(FirstName);
+            driver.FindElement(By.XPath("//input[@placeholder='Middle Name']")).SendKeys(MiddleName);
+            driver.FindElement(By.XPath("//input[@placeholder='Last Name']")).SendKeys(LastName);
+
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+            string Employee = driver.FindElement(By.CssSelector("[class='oxd-text oxd-text--h6 --strong']")).Text;
+            Console.WriteLine(Employee.ToCharArray());
+
+            Assert.That(ActualResult.Contains(Employee), "Employee details");
+
+            Console.WriteLine(ActualResult);
+
+          //  Assert.That(true);
+        }
+
+        
+    }
+}
---
+++ EmployeeManagement/Utilities/DataSource.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Utilities
+{
+    public class DataSource
+    {
+        public static object[] InvalidLoginData()
+        {
+            string[] dataSet1 = new string[3];   
+            dataSet1[0] = "john";
+            dataSet1[1] = "john123";
+            dataSet1[2] = "Invalid credentials";
+
+            string[] dataSet2 = new string[3];        
+            dataSet2[0] = "peter";
+            dataSet2[1] = "peter123";
+            dataSet2[2] = "Invalid credentials";
+
+            string[] dataSet3 = new string[3];           
+            dataSet3[0] = "saul";
+            dataSet3[1] = "saul123";
+            dataSet3[2] = "Invalid credentials";
+
+            object[] allDataSet = new object[3];  
+            allDataSet[0] = dataSet1;
+            allDataSet[1] = dataSet2;
+            allDataSet[2] = dataSet3;
+
+
+
+            return allDataSet;
+        }
+
+
+        public static object[] AddValidEmployeeTest1()
+        {
+            string[] AddData1 = new string[6];
+            AddData1[0] = "Admin";
+            AddData1[1] = "admin123";
+            AddData1[2] = "John";
+            AddData1[3] = "W";
+            AddData1[4] = "Wick";
+            AddData1[5] = "John Wick";
+
+            string[] AddData2 = new string[6];
+            AddData2[0] = "Admin";
+            AddData2[1] = "admin123";
+            AddData2[2] = "Saul";
+            AddData2[3] = "G";
+            AddData2[4] = "Goodman";
+            AddData2[5] = "Saul Goodman";
+
+            object[] AddAll = new object[2];
+           
+            AddAll[0] = AddData1;
+            AddAll[1]= AddData2;
+            return AddAll;
+        }
+
+    }
+}
